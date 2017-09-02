3fd93e8f44304463e52536c185b049a2d1d22554
Planetarium <31511257+khpldome@users.noreply.github.com>
2017-09-02 16:50:49 +0300

    xml parsing

--- dome/views.py
+++ dome/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import View, TemplateView
+import xmltodict
+import pprint
 
 
 from django.views import View
@@ -10,9 +12,7 @@ from django.views import View
 
 def index(request):
     context = {'latest_question_list': 22}
-    if request.method == 'GET':
-        start_mosaic()
-    return render(request, 'base.html', context)
+    return render(request, 'dome/index.html', context)
 
 
 class StoreDirectView(TemplateView):
@@ -34,17 +34,7 @@ class StoreDirectView(TemplateView):
             print("get=", mosaic_action)
 
             text_output = ""
-            if mosaic_action == "Start":
-                # messages.error(request, 'User already has tsdfghis module.')
-                text_output = start_mosaic()
-
-            elif mosaic_action == "Stop":
-                # messages.error(request, 'User alreaghfgdy has this module.')
-                text_output = start_mosaic()
-
-            elif mosaic_action == "Restart":
-                # messages.error(request, 'User alrefghfgady has this module.')
-                text_output = start_mosaic()
+            text_output = start_mosaic(mosaic_action)
 
         # context['pages'] = ModulePage.objects.filter(module_id=self.mymodule.id)
         context.update({
@@ -59,10 +49,7 @@ class StoreDirectView(TemplateView):
 
 
 
-
-
-
-def start_mosaic():
+def start_mosaic(mode):
     print("Stsrt mosaic")
     import os
     import ctypes
@@ -71,18 +58,36 @@ def start_mosaic():
     enc = 'cp%d' % ctypes.windll.kernel32.GetOEMCP()
     pathMosaic = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+r'\exec\Mosaic\configureMosaic-32bit-64bit.exe'
     pathVLC = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+r'\exec\vlc\vlc.exe'
-    MOSAIC_ENABLE = 'set rows=1 cols=8 res=1280,768,60 out=0,0 out=0,1 out=0,2 out=0,3 out=1,0 out=1,1 out=1,2 out=1,3'
-    MOSAIC_DISABLE = 'disable'
+    # MOSAIC_ENABLE = 'set rows=1 cols=8 res=1280,768,60 out=0,0 out=0,1 out=0,2 out=0,3 out=1,0 out=1,1 out=1,2 out=1,3'
+    # MOSAIC_DISABLE = 'disable'
 
-    # out = check_output(['ping', '8.8.8.8'])
+    if mode == "Start":
+        str_param = 'set rows=1 cols=8 res=1280,768,60 out=0,0 out=0,1 out=0,2 out=0,3 out=1,0 out=1,1 out=1,2 out=1,3'
+
+    elif mode == "Stop":
+        str_param = 'disable'
+
+    elif mode == "Restart":
+        str_param = 'help'
+
+    elif mode == "State":
+        str_param = 'query current'
+
     # out = check_output([pathVLC])
-    out = check_output([pathMosaic, 'help'])
+    # out = check_output([pathMosaic, str_param])
+    out = check_output([pathMosaic, 'query', 'current'])
+
+    doc_dict = xmltodict.parse(out)  # Parse the read document string
+    pprint.pprint(doc_dict)
+
+    print(doc_dict['query']['grids']['grid']['@rows'])
+    print(doc_dict['query']['grids']['grid']['@columns'])
     print(out.decode(enc))
 
     # print(sys.getdefaultencoding())
     # print(locale.getpreferredencoding())
     # print(sys.stdout.encoding)
     # print(sys.stderr.encoding)
-    print(pathMosaic)
+    # print(pathMosaic)
 
     return out.decode(enc)
