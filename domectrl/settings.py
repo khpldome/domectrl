7ca1394562b6f5ad7b6e768da1eef14483fd14cd
Planetarium <31511257+khpldome@users.noreply.github.com>
2017-09-02 16:03:34 +0300

    apachi started

--- domectrl/settings.py
+++ domectrl/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dome.apps.DomeConfig',
+    # 'mod_wsgi.server',    # for daemon mode
 ]
 
 MIDDLEWARE = [
--- domectrl/wsgi.py
+++ domectrl/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "domectrl.settings")
+sys.path.append('d:/PROJECTS/_python/domectrl')
+sys.path.append('d:/PROJECTS/_python/domectrl/domectrl')
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "domectrl.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "domectrl.settings"
 
 application = get_wsgi_application()
