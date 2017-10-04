4a878b236c71c4cb979958961f13e4487dca06a1
Planetarium <31511257+khpldome@users.noreply.github.com>
2017-10-04 19:11:10 +0300

    serial

---
+++ qweqweq/tst_serial.py
@@ -0,0 +1,20 @@
+
+
+import serial
+import time
+
+if __name__ == "__main__":
+
+    ser = serial.Serial('COM4', 9600, parity=serial.PARITY_NONE)
+    print(ser.name)
+    print(ser.write(b'hello'))
+
+    # s = ser.read(10)  # read up to ten bytes (timeout)
+
+        while ser.is_open:
+            line = ser.readline()  # read a '\n' terminated line
+            print(line)
+        time.sleep(10)
+    ser.close()
+
+    print("End")
