5b94d7e5d532da6e1155863d3423539a1fd5e228
Fulldome <khpldome@gmail.com>
2017-10-23 21:24:29 +0300

    Added vlc archive, django+apach

--- domectrl/settings.py
+++ domectrl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e=%ca7=6mhc^z@&l1=p+mv$k!-@y8mi#h%d17d@vie@(hv*93_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.101', 'localhost', '127.0.0.1', '192.168.0.109']
+ALLOWED_HOSTS = ['192.168.0.101', 'localhost', '127.0.0.1', '192.168.0.119']
 # ALLOWED_HOSTS = ['*']
 
 # Application definition
