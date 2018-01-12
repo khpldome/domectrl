7493005708d88d2f2a89b8ce5ab687b9bd982e3f
khpldome <khpldome@gmail.com>
2018-01-12 18:47:12 +0200

    browser in root

--- domeplaylist/urls.py
+++ domeplaylist/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url, include
 
 from . import views
 
+import filemanager
 
 app_name = "domeplaylist"
 
@@ -19,6 +20,8 @@ playlist_urls = [
     url(r'^playitem/(?P<playitem_id>\d+)/edit/$', views.EditPlayItemFormView.as_view(), name='edit_playitem'),
     url(r'^playitem/(?P<playitem_id>\d+)/delete/$', views.DeletePlayItemView.as_view(), name='delete_playitem'),
 
+    # url(r'^picker/', include('filemanager.urls', namespace="filemanager")),
+    # url(r'^picker/', filemanager.urls),
 ]
 
 
