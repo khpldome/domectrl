5125f5529f3625889e558379da54aa4d63a20982
HP_vm-PC\HP_vm <20flint12@gmail.com>
2018-01-12 22:46:30 +0200

    restored vm + db postgre

---
+++ domectrl/local_settings.py
@@ -0,0 +1,46 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7966t3rvn4ik7',
+        'USER': 'frwhgfyaaewlln',
+        'PASSWORD': '0a3d1f1ceff92f77b6c877148f527ced775c36117c2720f422dd2958fb6840c6',
+        'HOST': 'ec2-54-217-218-80.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
